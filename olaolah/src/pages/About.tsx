818f3403875d0c442078b6c59e3e625a30c1b8f1
mawasserman <wasserman.mv@gmail.com>
2024-12-08 17:13:01 +0200

    added the css of the about

--- olaolah/src/pages/About.tsx
+++ olaolah/src/pages/About.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const About: React.FC = () => {
     return (
-        <div>
+        <div className='homePage'>
             <h1>About Us</h1>
-            <p><b>Olá Olah</b> is a podcast hosted by two Brazilian, Orthodox Jewish moms navigating life, motherhood, and faith. Join them as they share heartfelt stories, cultural insights, and relatable experiences, all while balancing the joys and challenges of raising kids and embracing their unique identity.</p>
+            <p className='latestEpisode'>Olá Olah is a podcast hosted by two Brazilian, Orthodox Jewish moms navigating life, motherhood, and faith. Join them as they share heartfelt stories, cultural insights, and relatable experiences, all while balancing the joys and challenges of raising kids and embracing their unique identity.
+            <br/><br/>Marcella Wasserman is a mother of two kids and two cats. A geologist and diamond grader now persuing a new career, because her greatest hobby is the constant learning and sharing of knowledge. She met Malka during their years working together in the diamond industry.
+            <br/><br/>Malka is a mother of three kids no pets. She is a former designer and a current stay-at-home mom. She is a native Brazilian and Carioca-da-Gema. She met Marcella during their years working together in the diamond industry.
+            </p>
         </div>
     );
 }
